af90df88967c00eed7d6918d322cf3d820146b48
felipejsm <felipejsm41@gmail.com>
2018-07-15 15:38:31 -0300

    Displaying a list finished

---
+++ angular/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './Hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Wolverine' },
+    { id: 12, name: 'Storm' },
+    { id: 13, name: 'Hulk' },
+    { id: 14, name: 'Scott Pilgrim' },
+    { id: 15, name: 'All Might' },
+    { id: 16, name: 'Deku' },
+    { id: 17, name: 'Thor' },
+    { id: 18, name: 'Goku' },
+    { id: 19, name: 'Saitama' },
+    { id: 20, name: 'Chapolim' }
+];
